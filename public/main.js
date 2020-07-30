baa6b754520f3904b0705820e8764b20586d7063
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2020-07-30 09:58:47 +0530

    first commit

---
+++ public/main.js
@@ -0,0 +1,30 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({x: 200, y: 100, width: 1280, height: 720, resizable: false});
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.setMenuBarVisibility(false);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            Electron-React Appg
+        </div>
+    )
+}
+
+export default App
