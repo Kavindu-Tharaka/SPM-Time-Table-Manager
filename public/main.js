03a5d9512c677c00214e5b6b1ce15469fdf72515
Thamal Dilanka <thamaldilanke@gmail.com>
2020-08-03 15:18:31 +0530

    developer tool window enabled

--- public/main.js
+++ public/main.js
@@ -9,22 +9,33 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({x: 200, y: 100, width: 1440, height: 810, resizable: false});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  mainWindow.on('closed', () => mainWindow = null);
-  mainWindow.setMenuBarVisibility(false);
+	mainWindow = new BrowserWindow({
+		x: 200,
+		y: 100,
+		width: 1366,
+		height: 768,
+		resizable: false,
+	});
+	mainWindow.loadURL(
+		isDev
+			? 'http://localhost:3000'
+			: `file://${path.join(__dirname, '../build/index.html')}`
+	);
+	mainWindow.on('closed', () => (mainWindow = null));
+	mainWindow.setMenuBarVisibility(false);
+	mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
