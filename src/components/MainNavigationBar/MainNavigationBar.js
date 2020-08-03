5ea46f007a4f086adb8afcb551ece5d23e7df4d4
Thamal Dilanka <thamaldilanke@gmail.com>
2020-08-04 00:25:17 +0530

    main navigation bar modified to highlight the  selected link

--- src/components/MainNavigationBar/MainNavigationBar.js
+++ src/components/MainNavigationBar/MainNavigationBar.js
@@ -14,7 +14,7 @@ import {
 	FaExclamationTriangle,
 } from 'react-icons/fa';
 
-import './MainNavigationBar.css';
+import './mainNavigationBar.css';
 
 const MainNavigationBar = (props) => {
 	const iconSize = '24px';
@@ -23,7 +23,14 @@ const MainNavigationBar = (props) => {
 	return (
 		<div className='mnb-nav-container'>
 			<Link to='/working-time'>
-				<div className='mnb-nav-link-container'>
+				<div
+					className={`mnb-nav-link-container ${
+						props.selectedMainLink === 'working-time'
+							? 'mnb-nav-link-container-active'
+							: null
+					}`}
+					onClick={() => props.setSelectedMainLink('working-time')}
+				>
 					<FaRegCalendarAlt
 						size={`${iconSize}`}
 						color={`${iconColor}`}
@@ -33,7 +40,14 @@ const MainNavigationBar = (props) => {
 			</Link>
 
 			<Link to='/lecturers'>
-				<div className='mnb-nav-link-container'>
+				<div
+					className={`mnb-nav-link-container ${
+						props.selectedMainLink === 'lecturers'
+							? 'mnb-nav-link-container-active'
+							: null
+					}`}
+					onClick={() => props.setSelectedMainLink('lecturers')}
+				>
 					<FaChalkboardTeacher
 						size={`${iconSize}`}
 						color={`${iconColor}`}
@@ -43,14 +57,28 @@ const MainNavigationBar = (props) => {
 			</Link>
 
 			<Link to='/subjects'>
-				<div className='mnb-nav-link-container'>
+				<div
+					className={`mnb-nav-link-container ${
+						props.selectedMainLink === 'subjects'
+							? 'mnb-nav-link-container-active'
+							: null
+					}`}
+					onClick={() => props.setSelectedMainLink('subjects')}
+				>
 					<FaBook size={`${iconSize}`} color={`${iconColor}`} />
 					<p className='mnb-nav-link-text'>Subjects</p>
 				</div>
 			</Link>
 
 			<Link to='/student-groups'>
-				<div className='mnb-nav-link-container'>
+				<div
+					className={`mnb-nav-link-container ${
+						props.selectedMainLink === 'student-groups'
+							? 'mnb-nav-link-container-active'
+							: null
+					}`}
+					onClick={() => props.setSelectedMainLink('student-groups')}
+				>
 					<FaUserFriends
 						size={`${iconSize}`}
 						color={`${iconColor}`}
@@ -60,28 +88,56 @@ const MainNavigationBar = (props) => {
 			</Link>
 
 			<Link to='/tags'>
-				<div className='mnb-nav-link-container'>
+				<div
+					className={`mnb-nav-link-container ${
+						props.selectedMainLink === 'tags'
+							? 'mnb-nav-link-container-active'
+							: null
+					}`}
+					onClick={() => props.setSelectedMainLink('tags')}
+				>
 					<FaTags size={`${iconSize}`} color={`${iconColor}`} />
 					<p className='mnb-nav-link-text'>Tags</p>
 				</div>
 			</Link>
 
 			<Link to='/locations'>
-				<div className='mnb-nav-link-container'>
+				<div
+					className={`mnb-nav-link-container ${
+						props.selectedMainLink === 'locations'
+							? 'mnb-nav-link-container-active'
+							: null
+					}`}
+					onClick={() => props.setSelectedMainLink('locations')}
+				>
 					<FaBuilding size={`${iconSize}`} color={`${iconColor}`} />
 					<p className='mnb-nav-link-text'>Locations</p>
 				</div>
 			</Link>
 
 			<Link to='/sessions'>
-				<div className='mnb-nav-link-container'>
+				<div
+					className={`mnb-nav-link-container ${
+						props.selectedMainLink === 'sessions'
+							? 'mnb-nav-link-container-active'
+							: null
+					}`}
+					onClick={() => props.setSelectedMainLink('sessions')}
+				>
 					<FaClock size={`${iconSize}`} color={`${iconColor}`} />
 					<p className='mnb-nav-link-text'>Sessions</p>
 				</div>
 			</Link>
 
 			<Link to='/working-time'>
-				<div className='mnb-nav-link-container'>
+				<div
+					className={`mnb-nav-link-container ${
+						props.selectedMainLink === 'constraints'
+							? 'mnb-nav-link-container-active'
+							: null
+					}`}
+					onClick={() => props.setSelectedMainLink('constraints')}
+				>
 					<FaExclamationTriangle
 						size={`${iconSize}`}
 						color={`${iconColor}`}
@@ -91,14 +147,28 @@ const MainNavigationBar = (props) => {
 			</Link>
 
 			<Link to='/statistics'>
-				<div className='mnb-nav-link-container'>
+				<div
+					className={`mnb-nav-link-container ${
+						props.selectedMainLink === 'statistics'
+							? 'mnb-nav-link-container-active'
+							: null
+					}`}
+					onClick={() => props.setSelectedMainLink('statistics')}
+				>
 					<FaChartPie size={`${iconSize}`} color={`${iconColor}`} />
 					<p className='mnb-nav-link-text'>Statistics</p>
 				</div>
 			</Link>
 
 			<Link to='/timetables'>
-				<div className='mnb-nav-link-container'>
+				<div
+					className={`mnb-nav-link-container ${
+						props.selectedMainLink === 'timetables'
+							? 'mnb-nav-link-container-active'
+							: null
+					}`}
+					onClick={() => props.setSelectedMainLink('timetables')}
+				>
 					<FaTh size={`${iconSize}`} color={`${iconColor}`} />
 					<p className='mnb-nav-link-text'>Timetables</p>
 				</div>
