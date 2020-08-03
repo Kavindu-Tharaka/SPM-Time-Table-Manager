f5a25381ee12025a0be7af2d167324490dc5b114
Thamal Dilanka <thamaldilanke@gmail.com>
2020-08-03 09:26:59 +0530

    window resized

--- public/main.js
+++ public/main.js
@@ -9,7 +9,7 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({x: 200, y: 100, width: 1280, height: 720, resizable: false});
+  mainWindow = new BrowserWindow({x: 200, y: 100, width: 1440, height: 810, resizable: false});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
   mainWindow.setMenuBarVisibility(false);
