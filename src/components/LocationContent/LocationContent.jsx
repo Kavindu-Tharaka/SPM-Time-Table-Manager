e3d0d5d92feb51722ec0a95d3ce8d4868cc68a60
Thamal Dilanka <thamaldilanke@gmail.com>
2020-08-18 23:25:37 +0530

    input grid added

--- src/components/LocationContent/LocationContent.jsx
+++ src/components/LocationContent/LocationContent.jsx
@@ -18,8 +18,8 @@ const LocationContent = (props) => {
 				<input
 					type='text'
 					className='form-control'
-                    placeholder='Building Name'
-                    onChange={onBuildingNameChange}
+					placeholder='Building Name'
+					onChange={onBuildingNameChange}
 					value={buildingName}
 				/>
 				<button className='btn btn-primary form-element-left-margin'>
@@ -27,13 +27,39 @@ const LocationContent = (props) => {
 				</button>
 			</div>
 
-            <EmptyDataPlaceholder message='Building list is currently empty'/>
+			<EmptyDataPlaceholder message='Building list is currently empty' />
 
-            <BuildingCards/>
-
+			<BuildingCards />
 
 			<br />
 			<ContentHeader header='Rooms' label='All' />
+
+			<div>
+				<div className='row'>
+					<div className='col-3'>
+						<input
+							type='text'
+							className='form-control'
+							placeholder='Building Name'
+							onChange={onBuildingNameChange}
+							value={buildingName}
+						/>
+					</div>
+					<div className='col-3'>
+						<input
+							type='text'
+							className='form-control'
+							placeholder='Building Name'
+							onChange={onBuildingNameChange}
+							value={buildingName}
+						/>
+					</div>
+					<div className='col-1'>3 of 3</div>
+					<div className='col-1'>3 of 3</div>
+					<div className='col-3'>3 of 3</div>
+					<div className='col-1'>3 of 3</div>
+				</div>
+			</div>
 		</div>
 	);
 };
