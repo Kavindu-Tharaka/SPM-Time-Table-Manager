575f45fd35ccb004c7ac56cccf2320386025b36f
Thamal Dilanka <thamaldilanke@gmail.com>
2020-08-18 09:08:40 +0530

    Building initialized

--- src/components/LocationContent/LocationContent.js
+++ src/components/LocationContent/LocationContent.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContentHeader from '../ContentHeader/ContentHeader';
+import EmptyDataPlaceholder from '../EmptyDataPlacehoder/EmptyDataPlaceholder';
+import BuildingCards from '../BuildingCards/BuildingCards';
 
 const LocationContent = (props) => {
-    return (
-        <div>
-            <ContentHeader header='Buildings'/>
+	const [buildingName, setBuildingName] = useState('');
 
-            <ContentHeader header='Rooms' label='All'/>
-        </div>
-    );
+	const onBuildingNameChange = (e) => {
+		setBuildingName(e.target.value);
+	};
+
+	return (
+		<div>
+			<ContentHeader header='Buildings' />
+
+			<div className='single-input-container d-flex'>
+				<input
+					type='text'
+					className='form-control'
+                    placeholder='Building Name'
+                    onChange={onBuildingNameChange}
+					value={buildingName}
+				/>
+				<button className='btn btn-primary form-element-left-margin'>
+					Add
+				</button>
+			</div>
+
+            <EmptyDataPlaceholder message='Building list is currently empty'/>
+
+            <BuildingCards/>
+
+
+			<br />
+			<ContentHeader header='Rooms' label='All' />
+		</div>
+	);
 };
 
 export default LocationContent;
