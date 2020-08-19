ea98492927c16a163cfb1dc0c0fc05fcc1b3d075
Thamal Dilanka <thamaldilanke@gmail.com>
2020-08-19 23:01:11 +0530

    starting page issue fixed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
 				selectedMainLink={selectedMainLink}
 				setSelectedMainLink={setSelectedMainLink}
 			/>
-			<Redirect to='/working-time'/>
 			<ApplicationContent />
 		</BrowserRouter>
 	);
