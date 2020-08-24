573c7f9d6aa1ae870246211cded7ddbdeb533ee9
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2020-08-24 12:38:26 +0530

    initialized sprint 2 of member 1

---
+++ src/components/ConstraintsLecturers/ConstraintsLecturers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContentHeader from '../ContentHeader/ContentHeader';
+
+function ConstraintsLecturers() {
+    return (
+        <div>
+            <ContentHeader header={'Lecturers'} />
+        </div>
+    );
+}
+
+export default ConstraintsLecturers;
---
+++ src/components/ConstraintsUnOverlapSessions/ConstraintsUnOverlapSessions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContentHeader from '../ContentHeader/ContentHeader';
+
+function ConstraintsUnOverlapSessions() {
+    return (
+        <div>
+            <ContentHeader header={'Un-Overlap Sessions'} />
+        </div>
+    );
+}
+
+export default ConstraintsUnOverlapSessions;
