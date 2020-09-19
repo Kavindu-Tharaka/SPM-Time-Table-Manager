5a365be78ac7d42fd4267fbca0f9e1dd0d3d0c12
Thamal Dilanka <thamaldilanke@gmail.com>
2020-09-19 19:21:58 +0530

    sprint two base structure initialized

---
+++ src/components/AssignRooms/AssignForLecturers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assignRooms.css'
+
+const AssignForLecturers = (props) => {
+    return (
+        <div>
+            <h1>Assign For Lecturers</h1>
+        </div>
+    );
+};
+
+export default AssignForLecturers;
--- src/containers/Locations/Locations.jsx
+++ src/containers/Locations/Locations.jsx
@@ -1,15 +1,75 @@
+import { Switch, Route } from 'react-router-dom';
 import React, { Fragment } from 'react';
 import { useEffect } from 'react';
+
+// Component
+import AssignForTags from '../../components/AssignRooms/AssignForTags';
 import LocationContent from '../../components/LocationContent/LocationContent';
+import SubNavigationBar from '../../components/SubNavigationBar/SubNavigationBar';
+import SubNavLink from '../../models/SubNavLink';
+import AssignForSubjects from '../../components/AssignRooms/AssignForSubjects';
+import AssignForLecturers from '../../components/AssignRooms/AssignForLecturers';
+import AssignForGroups from '../../components/AssignRooms/AssignForGroups';
+import AssignForSessions from '../../components/AssignRooms/AssignForSessions';
+import AssignForConsecutive from '../../components/AssignRooms/AssignForConsecutive';
+import UnavailableTimes from '../../components/AssignRooms/UnavailableTimes';
 
 const Locations = (props) => {
+	const links = [
+		new SubNavLink('Buildings & Rooms', '/locations/locations-content', 'locations-content'),
+		new SubNavLink('Assign for Tags', '/locations/assign-for-tags', 'assign-for-tags'),
+		new SubNavLink('Assign for Subjects', '/locations/assign-for-subjects', 'assign-for-subjects'),
+		new SubNavLink('Assign for Lecturers', '/locations/assign-for-lecturers', 'assign-for-lecturers'),
+		new SubNavLink('Assign for Groups', '/locations/assign-for-groups', 'assign-for-groups'),
+		new SubNavLink('Assign for Sessions', '/locations/assign-for-sessions', 'assign-for-sessions'),
+		new SubNavLink('Assign for Consecutive', '/locations/assign-for-consecutive', 'assign-for-consecutive'),
+		new SubNavLink('Unavailable Times', '/locations/unavailable-times', 'unavailable-times'),
+	];
+
 	useEffect(() => {
-		props.setShowSubMenu(false);
+		props.setShowSubMenu(true);
 	});
 
 	return (
 		<Fragment>
-			<LocationContent/>
+			<SubNavigationBar links={links} header='Locations' />
+
+			<Switch>
+
+				<Route
+					path='/locations/assign-for-tags'
+					component={AssignForTags}
+				/>
+				<Route
+					path='/locations/assign-for-subjects'
+					component={AssignForSubjects}
+				/>
+				<Route
+					path='/locations/assign-for-lecturers'
+					component={AssignForLecturers}
+				/>
+				<Route
+					path='/locations/assign-for-groups'
+					component={AssignForGroups}
+				/>
+				<Route
+					path='/locations/assign-for-sessions'
+					component={AssignForSessions}
+				/>
+				<Route
+					path='/locations/assign-for-consecutive'
+					component={AssignForConsecutive}
+				/>
+				<Route
+					path='/locations/unavailable-times'
+					component={UnavailableTimes}
+				/>
+				<Route
+					path='/locations/'
+					component={LocationContent}
+				/>
+			</Switch>
+
 		</Fragment>
 	);
 };
