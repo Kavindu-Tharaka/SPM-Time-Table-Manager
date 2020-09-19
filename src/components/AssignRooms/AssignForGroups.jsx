d9b110abade79917b72bb6577b4ac6dc12cb64cc
Thamal Dilanka <thamaldilanke@gmail.com>
2020-09-19 19:21:58 +0530

    sprint two base structure initialized

---
+++ src/components/AssignRooms/AssignForGroups.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assignRooms.css'
+
+const AssignForGroups = (props) => {
+    return (
+        <div>
+            <h1>Assign For Groups</h1>
+        </div>
+    );
+};
+
+export default AssignForGroups;
---
+++ src/components/AssignRooms/AssignForTags.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assignRooms.css'
+
+const AssignForTags = (props) => {
+    return (
+        <div>
+            <h1>Assign for Tags</h1>
+        </div>
+    );
+};
+
+export default AssignForTags;
