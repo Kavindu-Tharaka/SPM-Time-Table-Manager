76bc3e814927137c92f0887af6e2207005e799d3
Thamal Dilanka <thamaldilanke@gmail.com>
2020-09-27 16:38:22 +0530

    function implemented for assign rooms for tags

--- src/components/RoomCard/RoomCard.jsx
+++ src/components/RoomCard/RoomCard.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import { useDrag } from 'react-dnd';
 import { ItemTypes } from '../../util/ItemTypes';
+import { store } from 'react-notifications-component';
+import { buildToast } from '../../util/toast';
+import axios from 'axios';
 
-import './roomCard.css'
+import './roomCard.css';
 
 const RoomCard = (props) => {
+	const name = props.room.roomName;
+	const assignedTags = props.room.assignedTags;
 
-    const name = props.name;
+	const assignRoom = (tag) => {
+		const tags = [...assignedTags];
+		const tagIds = [];
 
-    const assignRoom = (tag) => {
-        alert(`You dropped ${name} into ${tag}!`);
-    }
+		tags.forEach((t) => {
+			tagIds.push(t._id);
+		});
 
-    const [{ isDragging }, drag] = useDrag({
+		tagIds.push(tag);
+
+		axios
+			.patch(`http://localhost:8000/api/v1/rooms/${props.room._id}`, {
+				assignedTags: [...new Set(tagIds)],
+			})
+			.then((res) => {
+				store.addNotification(
+					buildToast('success', 'Success', 'Room Assigned')
+				);
+				props.refreshComponent();
+			})
+			.catch((err) => {
+				console.log(err.response);
+			});
+	};
+
+	const [{ isDragging }, drag] = useDrag({
 		item: { name, type: ItemTypes.RoomCard },
 		end: (item, monitor) => {
 			const dropResult = monitor.getDropResult();
@@ -23,15 +47,39 @@ const RoomCard = (props) => {
 		collect: (monitor) => ({
 			isDragging: monitor.isDragging(),
 		}),
-    });
+	});
 
-    const opacity = isDragging ? 0.3 : 1;
+	const opacity = isDragging ? 0 : 1;
 
-    return (
-        <div className='room-card-container' ref={drag} style={{ opacity }}>
-            <p>{name}</p>
-        </div>
-    );
+	return (
+		<div className='col p-1'>
+			<div className='room-card-container' ref={drag} style={{ opacity }}>
+				<h5 className='m-0'>{name}</h5>
+				<p className='m-0'>
+					<strong>Capacity</strong> {props.room.capacity} |{' '}
+					<strong>Floor</strong> {props.room.floor}
+				</p>
+				<p className='m-0'>
+					<strong>Building</strong> {props.room.building.buildingName}
+				</p>
+
+				<div className='d-inline'>
+					{props.room.assignedTags.length === 0 ? (
+						<p>Not Assigned</p>
+					) : null}
+
+					{props.room.assignedTags.map((tag) => (
+						<p
+							className='badge badge-pill badge-info mb-0 mr-1'
+							key={tag._id}
+						>
+							{tag.tagname.charAt(0)}
+						</p>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default RoomCard;
