34a4a2311b77d64305f1c8b28075614eef585d2f
dinuka shameera <dinukashameera0657@gmail.com>
2020-10-02 00:31:58 +0530

    time table generate impolementation

--- src/components/NewTimeTable/NewTimeTable.jsx
+++ src/components/NewTimeTable/NewTimeTable.jsx
@@ -1,12 +1,137 @@
-import React from 'react'
-import ContentHeader from '../ContentHeader/ContentHeader'
+import React, { useState, useEffect } from "react";
+import ContentHeader from "../ContentHeader/ContentHeader";
+import Tag from "../Tag/Tag";
+import axios from "axios";
+import PreLoader from "../PreLoader/PreLoader";
 
 function NewTimeTable() {
-    return (
-        <div>
-            <ContentHeader header={'Generate Time Table'} />
-        </div>
-    )
+  const [yearsemesters, setYearSemester] = useState([]);
+  const [lecturers, setLecturers] = useState([]);
+  const [sessions, setSessions] = useState([]);
+  const [halls, setHalls] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getSemester();
+    getYear();
+    getLecturers();
+    gethalls();
+  }, []);
+
+  const getSemester = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.get("http://localhost:8000/api/v1/session");
+      console.log(result.data.data);
+      setSessions(result.data.data.sessions);
+      setLoading(false);
+    } catch (error) {
+        setLoading(false);
+    }
+  };
+
+  const getYear = async () => {
+    setLoading(true);
+
+    try {
+      const result = await axios.get(
+        "http://localhost:8000/api/v1/yearsemesters"
+      );
+      console.log(result.data.data.yearsemesters);
+      setYearSemester(result.data.data.yearsemesters);
+      setLoading(false);
+    } catch (error) {
+        setLoading(false);
+    }
+  };
+
+  const getLecturers = async () => {
+    setLoading(true);
+
+    try {
+      const result = await axios.get("http://localhost:8000/api/v1/lecturers");
+      console.log(result.data.data.lecturers);
+      setLecturers(result.data.data.lecturers);
+      setLoading(false);
+    } catch (error) {
+        setLoading(false);
+    }
+  };
+
+  const gethalls = async () => {
+    setLoading(true);
+
+    try {
+      const result = await axios.get("http://localhost:8000/api/v1/rooms");
+      console.log(result.data.data.rooms);
+      setHalls(result.data.data.rooms);
+      setLoading(false);
+    } catch (error) {
+        setLoading(false);
+    }
+  };
+
+
+  const onGenerate = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.post('http://localhost:8000/api/v1/timetable/lecturer',{
+        year : ['Y1','Y2','Y3','Y4'],
+        semester :['S1','S2'],
+        department :['SE','IT','DS','CS','ISE']
+      })
+      setLoading(false);
+      console.log(result)
+    } catch (error) {
+      
+    }
+  }
+
+
+  return (
+    <div>
+      <PreLoader loading={loading} />
+
+      <ContentHeader header={"Generate Time Table"} />
+      <div
+        style={{
+          justifyContent: "center",
+          textAlign: "center",
+          marginTop: "5%",
+          padding: "10px",
+          overflowY: "auto",
+        }}
+        className="row"
+      >
+        {yearsemesters.length > 0 ? (
+          <Tag id={"1"} deleteMethod={() => {}} tagName={"Year"} />
+        ) : null}
+        {lecturers.length > 0 ? (
+          <Tag id={"1"} deleteMethod={() => {}} tagName={"Lecturer"} />
+        ) : null}
+        {sessions.length > 0 ? (
+          <Tag id={"1"} deleteMethod={() => {}} tagName={"Sessions"} />
+        ) : null}
+        {halls.length > 0 ? (
+          <Tag id={"1"} deleteMethod={() => {}} tagName={"Hall"} />
+        ) : null}
+
+        {/* <Tag id={"1"} deleteMethod={() => {}} tagName={"kdhsabdhbsa"} /> */}
+      </div>
+      <div
+        style={{
+          justifyContent: "center",
+          textAlign: "center",
+          marginTop: "5%",
+          padding: "10px",
+          overflowY: "auto",
+        }}
+        className="row"
+      >
+        <button className="btn btn-primary" onClick={onGenerate}>Generate</button>
+      </div>
+    </div>
+  );
 }
 
-export default NewTimeTable
+export default NewTimeTable;
--- src/components/NewTimeTable/StudentTimeTable/StudentTimeTbale.jsx
+++ src/components/NewTimeTable/StudentTimeTable/StudentTimeTbale.jsx
@@ -4,13 +4,16 @@ import EmptyDataPlaceholder from "../../EmptyDataPlacehoder/EmptyDataPlaceholder
 import { data_ } from "../../../containers/dummydata";
 import axios from "axios";
 import DataTable from "react-data-table-component";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import PreLoader from "../../PreLoader/PreLoader";
 
 function StudentTimeTbale({ days }) {
   const [data, setData] = useState();
   const [year, setYear] = useState();
   const [department, setDepartment] = useState();
   const [semester, setSemester] = useState();
-
+const[loading,setLoading] = useState(false)
   // useEffect(() => {
   //   getTimeTableDetails();
   // }, []);
@@ -23,17 +26,19 @@ function StudentTimeTbale({ days }) {
     console.log(year);
     console.log(department);
     console.log(semester);
+    setLoading(true)
     try {
       const result = await axios.post(
         "http://localhost:8000/api/v1/timetable",
         {
           year: year,
-          department:department,
-          semester:semester
+          department: department,
+          semester: semester,
         }
       );
       console.log(result?.data);
       setData(result?.data);
+      setLoading(false)
     } catch (error) {
       console.log(error);
     }
@@ -54,8 +59,25 @@ function StudentTimeTbale({ days }) {
     setSemester(e.target.value);
   };
 
+  const printDocument = () => {
+    const input = document.getElementById("pdf");
+    html2canvas(input).then((canvas) => {
+      var imgWidth = 200;
+      var pageHeight = 290;
+      var imgHeight = (canvas.height * imgWidth) / canvas.width;
+      var heightLeft = imgHeight;
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4");
+      var position = 0;
+      var heightLeft = imgHeight;
+      pdf.addImage(imgData, "JPEG", 0, position, imgWidth, imgHeight);
+      pdf.save("download.pdf");
+    });
+  };
+
   return (
     <div>
+        <PreLoader loading={loading} hasSideBar={true} />
       <ContentHeader header={"Student Time Table"} />
       <div className="form-row" style={{}}>
         <div className="col-md-2 mb-3">
@@ -63,11 +85,11 @@ function StudentTimeTbale({ days }) {
         </div>
         <div className="col-md-2 mb-3">
           <select onChange={(e) => onDepartmentChange(e)}>
-            <option value='SE'>Software Engineering</option>
-            <option value='DS'>Data Science</option>
-            <option value ='IT'>Information Technology</option>
-            <option value='CS'>Cyber Security</option>
-            <option value ='ISE'>Information Systems Engineering</option>
+            <option value="SE">Software Engineering</option>
+            <option value="DS">Data Science</option>
+            <option value="IT">Information Technology</option>
+            <option value="CS">Cyber Security</option>
+            <option value="ISE">Information Systems Engineering</option>
           </select>
         </div>
         <div className="col-md-2 mb-3">
@@ -96,17 +118,26 @@ function StudentTimeTbale({ days }) {
           />
           Semester 2
         </div>
-        <div className="col-md-2 mb-3">
+        <div className="col-md-1 mb-3" style={{ marginRight: "10px" }}>
           <button className="btn btn-primary" type="submit" onClick={onSubmit}>
-            Add
+            View
+          </button>
+        </div>{" "}
+      </div>
+      <div className=" mb-3">
+        <div>
+          <button className="btn btn-primary" onClick={printDocument}>
+            Print
           </button>
         </div>
       </div>
-      <DataTable
-        columns={days}
-        noDataComponent={<EmptyDataPlaceholder message={"No Data Found"} />}
-        data={data}
-      />
+      <div id="pdf" >
+        <DataTable
+          columns={days}
+          noDataComponent={<EmptyDataPlaceholder message={"No Data Found"} />}
+          data={data}
+        />
+      </div>
     </div>
   );
 }
